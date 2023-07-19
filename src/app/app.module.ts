652dd0a5061daf96ed16cd63c1cd8ddc1bd000a3
Roman Tkachuk <tkachukrr@gmail.com>
2023-07-19 20:31:05 +0200

    001 Generated search-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchListComponent } from './search-list/search-list.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchListComponent
+    SearchListComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
