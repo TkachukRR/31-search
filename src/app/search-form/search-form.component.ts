074b2ed860146064eba0970cb94fd6d08c47c269
Roman Tkachuk <tkachukrr@gmail.com>
2023-07-19 20:59:31 +0200

    001 Added searching form

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-search-form',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-form.component.scss']
 })
 export class SearchFormComponent {
+  private _formBuilder = inject(FormBuilder);
 
+  public searchingForm = this._formBuilder.nonNullable.group({
+      value: this._formBuilder.nonNullable.control<string>(''),
+      date: this._formBuilder.nonNullable.control<number>(0)
+    }
+  )
+
+  public onSubmit(){
+    this.setActualFormDate()
+  }
+
+  private setActualFormDate(){
+    this.searchingForm.controls.date.patchValue(Date.now())
+  }
 }
