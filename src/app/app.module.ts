0ce9d592298d1a2c6f017ec2a01a5a337f43e120
Roman Tkachuk <tkachukrr@gmail.com>
2023-07-19 09:32:06 +0200

    001 generated search list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchListComponent } from './search-list/search-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchListComponent
   ],
   imports: [
     BrowserModule,
