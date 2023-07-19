7aec1c1991702d4f1fa114cd378bd854ebdef21f
Roman Tkachuk <tkachukrr@gmail.com>
2023-07-19 22:09:23 +0200

    001 implemented ability add new serch to local storage

--- src/app/services/search-storage.service.ts
+++ src/app/services/search-storage.service.ts
@@ -1,11 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { LocalStorageService } from "./local-storage.service";
 import { Searching } from "./interfaces";
+
+const LOCAL_STORAGE_KEY = "SEARCH_LIST"
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class SearchStorageService {
+  private _localStorage = inject(LocalStorageService);
+
   public addNewSearch(search: Searching): void{
+    const searchList = this._localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (!searchList) this._localStorage.setItem(LOCAL_STORAGE_KEY, [search])
+    else this._localStorage.setItem(LOCAL_STORAGE_KEY, [search, ...searchList])
   }
 }
